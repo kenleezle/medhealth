3d92c7160045086a108af80713bbe2a6d7017b58
Ken Leland III <k3leland@monmouth.com>
2014-02-15 18:12:53 +0400

    Integrated Prescription Button
    Still Bugs in Add Time

--- MedHealth/src/com/Hackathon/medhealth/MainActivity.java
+++ MedHealth/src/com/Hackathon/medhealth/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
@@ -29,12 +30,29 @@ public class MainActivity extends Activity
 	NotificationCompat.Builder	Activity_notification;
 	Intent						Activity_intent;
 	PendingIntent				Activity_pendingintent;
-	Button t;
+	ImageButton t;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.homepage);
+
+	    t = (ImageButton) findViewById(R.id.imageButton2);
+	   
+	    t.setOnClickListener(new OnClickListener()
+	    {
+
+	      @Override
+	      public void onClick(View v)
+	      {
+	        Intent i = new Intent(getApplicationContext(), New_Medicine.class);
+	        startActivity(i);
+
+	      }
+	    });
+	   
+	    Intent intent = new Intent(this, Service_Medicine_time.class);
+	    alarmIntent = PendingIntent.getService(this, 0, intent, 0);
 	}
 }
